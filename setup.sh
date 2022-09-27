2252d6c788ee8fee43568aea4fefe5783b4df3c9
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-09-27 23:19:36 +0300

    Add using of synproxy

--- setup.sh
+++ setup.sh
@@ -28,3 +28,12 @@ iptables -A INPUT -p tcp -m connlimit --connlimit-above 10 -j REJECT --reject-wi
 # limits the new tcp connections that a client can establish per second
 iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 10/s --limit-burst 5 -j ACCEPT 
 iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+
+# use synproxy on all ports
+iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
+iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+# protect ssh from brute-force
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 120 --hitcount 30 -j DROP
