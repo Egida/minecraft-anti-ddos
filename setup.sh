0b569a6462503956a44ef38215356bf3b194e864
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-09-28 01:50:29 +0300

    Change project structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-#      __   __               _                  
-#  ___/ /__/ /__  ___   ____(_)__  ___  ___ ____
-# / _  / _  / _ \(_-<  / __/ / _ \/ _ \/ -_) __/
-# \_,_/\_,_/\___/___/ /_/ /_/ .__/ .__/\__/_/   
-#                          /_/  /_/             
+#      __   __               _                   
+#  ___/ /__/ /__  ___   ____(_)__  ___  ___ ____ 
+# / _  / _  / _ \(_-<  / __/ / _ \/ _ \/ -_) __/ 
+# \_,_/\_,_/\___/___/ /_/ /_/ .__/ .__/\__/_/    
+#                          /_/  /_/              
 #
 # created by oqo0 | 2022 
 # 
@@ -14,122 +13,50 @@
 # on servers with hundreds of regular players. If you encounter any problems please create an issue on
 # project's Github: https://github.com/oqo0/minecraft-anti-ddos
 # 
-# It is also quite important to note that if your hosting provider does not have DDOS protection, then
-# this script will be practically useless.
-
-
-# Block dangerous activity
+# It is also quite important to note that if your hosting provider does not have any common anti-DDOS,
+# then this script will be practically useless.
 #
-# These rules are supposed to block invalid \ dangerous traffic from users.
-# Not recommended to remove any rules from here.
+# Important!
+# If you have already ran this script and you want to make some changes to protection, run ./tear-down
+# script first.
 
-# block invalid packets
-iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+######################################################################################################
 
-# block xmas packets (kamikaze packets)
-iptables -t mangle -A PREROUTING -p tcp -m tcp --tcp-flags ALL ALL -j DROP
+# Max amount of connections that can be established from and ip at the same time.
+max_established_connections=10
 
-# block null packets
-iptables -t mangle -A PREROUTING -p tcp -m tcp --tcp-flags ALL NONE -j DROP 
+# Max amount of new connections allowed in 1 minute.
+max_connections_per_second=80
 
-# block not syn packets
-iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
-
-# block uncommon MMS values
-iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
-
-# block all icmp packets
-iptables -t mangle -A PREROUTING -p icmp -j DROP
-
-# drop fragments in all chains
-iptables -t mangle -A PREROUTING -f -j DROP
-
-# limit rst packets
-iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
-
-# block packets with bogus tcp flags
-iptables -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
-iptables -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-
-# block spoofed packets
-iptables -A PREROUTING -s 224.0.0.0/3 -j DROP
-iptables -A PREROUTING -s 169.254.0.0/16 -j DROP
-iptables -A PREROUTING -s 172.16.0.0/12 -j DROP
-iptables -A PREROUTING -s 192.0.2.0/24 -j DROP
-iptables -A PREROUTING -s 192.168.0.0/16 -j DROP
-iptables -A PREROUTING -s 10.0.0.0/8 -j DROP
-iptables -A PREROUTING -s 0.0.0.0/8 -j DROP
-iptables -A PREROUTING -s 240.0.0.0/5 -j DROP
-iptables -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+# Rcon port you are using. Don't change this if you don't use rcon.
+rcon_port=999999
 
 
-# Traffic protection
-# 
-# The main task of this section is to reduce the load on the server during peak loads.
-# You can adjust some values.
+# Allow traffic to your Minecraft port only from whitelisted countries.
+geo_whitelist_enabled=false
 
-# block port scan
-sudo iptables -N anti-port-scan
-sudo iptables -A anti-port-scan -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j RETURN
-sudo iptables -A anti-port-scan -j DROP
+# Your Minecraft port.
+geo_minecraft_port=25565
 
-# block hosts that have more than 10 established connections
-iptables -A INPUT -p tcp -m connlimit --connlimit-above 10 -j REJECT --reject-with tcp-reset
-# limits the new tcp connections that a client can establish per second
-iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 10/s --limit-burst 5 -j ACCEPT 
-iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
-
-# use synproxy on all ports
-iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
-iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
-iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
-
-# protect ssh from brute-force
-iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
-iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 120 --hitcount 30 -j DROP
-
-# protect rcon from brute-force
-# enter your rcon port or remove this if you don't use it
-rcon_port=21000
-iptables -A INPUT -p tcp --dport $rcon_port -m conntrack --ctstate NEW -m recent --set
-iptables -A INPUT -p tcp --dport $rcon_port -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 20 -j DROP
+# Whitelisted countries
+# Uses country TLD: https://gist.github.com/oqo0/47a185af30c966a362dbdfebf3771400
+geo_whitelist_countries="us,uk,fr,de"
 
 
-# Geo protection
-# 
-# You can use this section to whitelist traffic from specific countries \ regions.
-# This option is disabled by default.
-# It is recommended to enable after you change the list of countries to the desired one.
+# Please check scripts/advanced_protection.sh before enabling
+# Disabled by default because it might cause some problems with incoming minecraft connections.
+enable_advanced_protection=false
 
-# enable geo protection
-enable_geo_protection=false
+######################################################################################################
 
-# set your minecraft port
-minecraft_port=25565
+./scripts/activity_protection.sh # Block dangerous activity
 
-# whitelisted countries
-whitelisted_countries="us,uk,fr"
+./scripts/traffic_protection.sh $max_established_connections $max_connections_per_second $rcon_port # Traffic protection
 
-# get country ip blocks
-country_list="https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/"
+if [ "$geo_whitelist_enabled" = true ] ; then
+    ./scripts/geo_protection.sh $geo_minecraft_port $geo_whitelist_countries # Geo protection
+fi
 
-# add whitelisted countries to an ipset
-for ip in $(curl -L $country_list/{$whitelisted_countries}.cidr);
-    do ipset -A county_whitelist $ip
-done
-
-# allow connections only from whitelisted countries
-iptables -A INPUT -m set --match-set county_whitelist src -p tcp --dport $minecraft_port -j ACCEPT
-iptables -A INPUT -p tcp --dport $minecraft_port -j DROP
+if [ "$enable_advanced_protection" = true ] ; then
+    ./scripts/advanced_protection.sh # Blocks somhow-invalid \ dangerous traffic.
+fi
