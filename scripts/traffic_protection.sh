357648db634bc879cc243065384988a14317f657
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-09-28 14:55:12 +0300

    Remove useless rule

--- scripts/traffic_protection.sh
+++ scripts/traffic_protection.sh
@@ -11,9 +11,6 @@ iptables -A INPUT -p tcp -m connlimit --connlimit-above $1 -j REJECT --reject-wi
 iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst $2 -j ACCEPT 
 iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
 
-# block syn-flood
-iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
-
 # use synproxy on all ports
 iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
 iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
